41b86a522b416fdf4c0bf681a43a59d8937d5846
Shazeb Suhail <43024685+shazebs@users.noreply.github.com>
2023-11-19 21:47:18 -0800

    Update spawnexample.c

--- spawnexample.c
+++ spawnexample.c
@@ -5,27 +5,32 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <spawn.h>
+
 extern char **environ;
+
 int main(int argc, char *argv[]){
   if (argc < 2){
     fprintf(stderr, "Usage :%s <path-to-executable>\n", argv[0]);
     return EXIT_FAILURE;
-  }
+  }  
   pid_t pid;
   int status;
   char cmd[100] = "/bin/";
   strncat(cmd, argv[1], 95);
   char *args[]={cmd, NULL};
+  
   if (posix_spawn(&pid, cmd, NULL, NULL, args, environ) != 0){
     printf("posix_spawn failed to execute command=%s\n", cmd);
     return EXIT_FAILURE;
   }
   sleep(1);
   printf("Spawned %s command with PID=%d\n", cmd, pid);
+  
   if (waitpid(pid, &status, 0) == -1){
     perror("waitpid failed");
     return EXIT_FAILURE;
   }
+  
   if (WIFEXITED(status)){
     printf("Process exited with status=%d\n", WEXITSTATUS(status));
   } else {
