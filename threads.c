f986360fbd64b838ecd22b76d66a2d12ce2575b2
Shazeb Suhail <43024685+shazebs@users.noreply.github.com>
2023-11-19 21:47:50 -0800

    Update threads.c

--- threads.c
+++ threads.c
@@ -23,10 +23,11 @@ void* thread_2(void* arg){
 void main(int argc, char **argv){
   pthread_t tids[2];
   pthread_attr_t attr;
-  pthread_attr_init(&attr);
+  pthread_attr_init(&attr);  
   int tresults[2];
   tresults[0] = pthread_create(&tids[0], &attr, thread_1, NULL);  // start thread 1
   tresults[1] = pthread_create(&tids[1], &attr, thread_2, NULL);  // start thread 2
+  
   for (int t=0; t < 2; t++){
     pthread_join(tids[t], NULL);  // wait for thread to end.
     printf("pthread tid=%d has ended with result=%d.\n", (int)tids[t], tresults[t]);
